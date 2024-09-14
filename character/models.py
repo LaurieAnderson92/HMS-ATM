b0cbb79f4cb36ee963c98759dd3b08c40f235b82
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-09-14 19:03:28 +0000

    build: created PostgreSQL db

--- character/models.py
+++ character/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+CHARACTER_STATUS = ((0, "Draft"), (1, "Active"))
 
 # Create your models here.
+class Character(models.Model):
+    name = models.CharField()
+    body = models.TextField()
+    player = models.ForeignKey(
+        User, related_name='character')
+    status = models.IntegerField(choices=CHARACTER_STATUS, default=0)
+    is_npc = models.BooleanField(default=False)
+    is_hidden = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} ({user.username})"
+    
