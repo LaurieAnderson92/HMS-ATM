ea56a15fca894569b3d253316b882efe71305e14
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-09-15 09:46:36 +0000

    build: added environ secret key

--- hms_app/settings.py
+++ hms_app/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f+xz)4+iob3=y)k#&s&b!f!uc1!x5vjl*7+#c+i7&25@=ilq35'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-laurieanderson92-hmsatm-is0o7b41b00.ws.codeinstitute-ide.net',]
 
